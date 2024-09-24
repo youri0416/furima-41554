bb97beed57b3b13c3da6aa3a9aeae29ac58a669c
youri0416 <withuuuu0416@gmail.com>
2024-09-24 15:21:26 +0900

    パスワードのバリデーション修正
    presence: trueを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password, presence: true, format: { with: /\A(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}+\z/ }
+  validates :password, format: { with: /\A(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}+\z/ }
   validates :nickname, presence: true
   validates :last_name, presence: true, format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}]+\z/ }
@@ -12,6 +12,6 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true, format: { with: /\A[\p{Katakana}ー]+\z/ }
   validates :birthday, presence: true
 
-  has_many :items
-  has_many :orders
+  # has_many :items
+  # has_many :orders
 end
