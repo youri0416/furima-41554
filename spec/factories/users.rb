136ac01a0332c87f6620249fcfbd2c65605c4ce1
youri0416 <withuuuu0416@gmail.com>
2024-09-24 15:20:36 +0900

    factorybot修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Internet.username }
     email { Faker::Internet.email }
-    password { "1a#{Faker::Internet.password(min_length: 6)}" }
+    password { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     last_name { '山田' }
     first_name { '花子' }
