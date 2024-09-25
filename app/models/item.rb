1a5cfad07cc8a73a984f9c79c1d9dfb17a4d7cff
youri0416 <withuuuu0416@gmail.com>
2024-09-25 20:35:14 +0900

    アクティブハッシュのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,4 +12,11 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :shipping_day
+  belongs_to :shipping_fee
 end
