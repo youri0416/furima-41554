5a5d0075c3814d93f2f968d98ecf079fc3a5f4b1
youri0416 <withuuuu0416@gmail.com>
2024-09-28 15:33:12 +0900

    purchaseテーブルのマイグレーション実行

---
+++ db/migrate/20240928061548_create_purchases.rb
@@ -0,0 +1,15 @@
+class CreatePurchases < ActiveRecord::Migration[7.0]
+  def change
+    create_table :purchases do |t|
+      t.string      :post_code,       null: false
+      t.integer     :prefecture_id,   null: false
+      t.string      :city,            null: false
+      t.string      :street_address,  null: false
+      t.string      :building
+      t.string      :phone_number,    null: false
+      t.references  :order,           null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
