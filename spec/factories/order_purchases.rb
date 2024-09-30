d4845001d808564b8d77d5417eee81a44845ec7b
youri0416 <withuuuu0416@gmail.com>
2024-09-30 21:38:18 +0900

    faker修正

--- spec/factories/order_purchases.rb
+++ spec/factories/order_purchases.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :order_purchase do
-    user_id { Faker::Number.non_zero_digit }
-    item_id { Faker::Number.non_zero_digit }
     post_code { "#{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}" } # ここを修正
     prefecture_id { Faker::Number.between(from: 2, to: 47) }
     city { Faker::Address.city }
