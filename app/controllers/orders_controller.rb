1c24cb534e6ab9fec0488787f579dc820999554d
youri0416 <withuuuu0416@gmail.com>
2024-09-30 20:11:12 +0900

    クレカ決済のため、コントローラー編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,40 @@
 class OrdersController < ApplicationController
+  before_action :set_item
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    @order_purchase = OrderPurchase.new
   end
 
   def create
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    @order_purchase = OrderPurchase.new(order_params)
+    if @order_purchase.valid?
+      pay_item
+      @order_purchase.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_purchase).permit(:post_code, :prefecture_id, :city, :street_address, :building, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price, # 商品の値段
+      card: order_params[:token], # カードトークン
+      currency: 'jpy' # 通貨の種類（日本円）
+    )
   end
 end
